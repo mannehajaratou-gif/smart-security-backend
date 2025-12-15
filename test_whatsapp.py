eed2def094d666294ee5f9b48623c172253e6f95
Hajarah <mannehajaratou@gmail.com>
2025-12-15 19:30:48 +0000

    Update test_whatsapp.py

--- test_whatsapp.py
+++ test_whatsapp.py
@@ -3,6 +3,6 @@ from whatsapp_alert import send_whatsapp_alert
 
 if __name__ == "__main__":
     # Replace with your WhatsApp number including country code
-    your_number = "+918827886523"  
+    your_number = "+91xxxxxxxxxx"  
 
     send_whatsapp_alert(your_number, "This is a test WhatsApp alert from your Smart Security System.")
